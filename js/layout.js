3027a12bcff2f9a28ce62271d64ac7ae5536bcb7
letan-165 <2251160057@ut.edu.vn>
2025-10-29 21:15:10 +0700

    chore: clean

--- js/layout.js
+++ js/layout.js
@@ -15,3 +15,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch("/html/layout/header.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+});
+
+fetch("/html/layout/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
