0e1bd18a13dc6e09f4d994466d0cbc402a514ed3
letan-165 <2251160057@ut.edu.vn>
2025-12-01 14:25:24 +0700

    refactor: deviceId

--- js/stats.js
+++ js/stats.js
@@ -4,6 +4,12 @@
   const ua = navigator.userAgent;
   const device = /Mobile|Android|iPhone|iPad/i.test(ua) ? "Mobile" : "PC";
 
+  let deviceId = localStorage.getItem("deviceId");
+  if (!deviceId) {
+    deviceId = crypto.randomUUID();
+    localStorage.setItem("deviceId", deviceId);
+  }
+
   fetch("https://ipwho.is/")
     .then((res) => res.json())
     .then((data) => {
@@ -12,15 +18,18 @@
       const region = data.region || "Không có";
       const country = data.country || "Không có";
 
+      const visitData = {
+        id: deviceId,
+        lastTime: timeNow,
+        ip: ip,
+        area: `${city}, ${region}, ${country}`,
+        device: device,
+      };
+
       fetch("https://noble-debee-tandev-06be2084.koyeb.app/stats/visit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          lastTime: timeNow,
-          ip: ip,
-          area: `${city}, ${region}, ${country}`,
-          device: device,
-        }),
+        body: JSON.stringify(visitData),
       })
         .then((res) => res.json())
         .then((result) => console.log("Stats sent:", result))
@@ -29,11 +38,13 @@
     .catch(() => {
       console.warn("Không lấy được IP/khu vực, vẫn gửi device + time");
       const visitData = {
+        id: deviceId,
         lastTime: timeNow,
         ip: "Không có",
         area: "Không có",
         device: device,
       };
+
       fetch("https://noble-debee-tandev-06be2084.koyeb.app/stats/visit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
